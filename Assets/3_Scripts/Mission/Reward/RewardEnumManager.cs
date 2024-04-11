4a2c074534c2311bea26f495417b61ac2c3ab83e
RezaRahimi2 <Reza.Rahimi.2@gmail.com>
2024-04-11 10:20:55 +0300

    Commit Project

--- Assets/3_Scripts/Mission/Reward/RewardEnumManager.cs
+++
@@ -1,8 +0,0 @@
-﻿    using System;
-    using UnityEngine;
-
-    [CreateAssetMenu(fileName = "RewardList", menuName = "MissionsData/Reward List", order = 1)]
-    public class RewardEnumManager:ScriptableObject
-    {
-        public Enum RewardTypeEnum;
-    }
--- Assets/3_Scripts/PrefabManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using Mission;
-using UnityEngine;
-
-public class PrefabManager : Singleton<PrefabManager>
-{
-    public MissionManager MissionManagerPrefab;
-    public TowerTile TilePrefab;
-}
