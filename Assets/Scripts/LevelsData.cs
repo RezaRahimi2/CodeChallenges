3d8d40b6858c3a527a0867ff9591f753d2d705c7
RezaRahimi2 <Reza.Rahimi.2@gmail.com>
2024-04-13 02:26:38 +0300

    Initial Commit

--- Assets/Scripts/LevelsData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "LevelsData", menuName = "LevelsData", order = 1)]
-public class LevelsData : ScriptableObject
-{
-    public List<GameObject> LevelsPrefab;
-    
-}
